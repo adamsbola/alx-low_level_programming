924b569f8421bc2a3bad19be9c9cc1e7ce80e4cd
adamsbola <adamadu52@gmail.com>
2022-09-09 08:57:47 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
-
-
+# include <stdio.h>
 
 /**
  *
  *  * main - Entry point
  *
- *   *
+ *   * My hello, print task
  *
- *    * Return: Always 0 (Success)
+ *    * Return: return 1 after program execution
  *
  *     */
 
@@ -16,8 +14,8 @@ int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+	  return (0);
 
 }
