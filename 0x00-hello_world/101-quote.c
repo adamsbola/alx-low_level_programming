708f7ed66376ca87780e3bc84cf24b99c569dfcb
adamsbola <adamadu52@gmail.com>
2022-09-10 18:30:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <unistd.h>
 
 /**
@@ -10,8 +8,7 @@
  *   * Return: Always 1 (Success)
  *
  */
-int main(void)
-	
-{		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+int main(void)	
+{		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);	
+	return (1);
 }
