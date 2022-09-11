fe32249feb37f20d31f1af6071a35cd9b5e297ea
adamsbola <adamadu52@gmail.com>
2022-09-10 18:14:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,11 @@
 
 
 /**
- *
  *  * main - Entry point
  *
  *   * Return: Always 1 (Success)
  *
- *    */
+ */
 
 int main(void)
 
@@ -21,5 +20,4 @@ int main(void)
 		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
-
 }
