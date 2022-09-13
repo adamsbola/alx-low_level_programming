dc9a65543472269640d3e4ca5f92533f926e76ba
adamsbola <adamadu52@gmail.com>
2022-09-13 09:10:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * main - print putchar
- * Return: always 0
+ *  main - print putchar
+ *  Return: always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +14,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
+	
 	return (0);
-}
+}	
