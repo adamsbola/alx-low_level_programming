ecbf0a44206e35e444ceafd0173c3062387cdead
adamsbola <adamadu52@gmail.com>
2022-09-12 17:48:21 -0700

    10 1-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #inclure <stdio.h>
 #inclure <stdlib.h>
 #inclure <unistd.h>
+
 /**
  *principal - Point d’entrée
  *Retour: Toujours 1 (Succès)
  */
 int main(void)
 {
-write(STDOUT_FILENO, « et cette œuvre d’art est utile\ » - Dora Korpar,
+write(2, "and that piece of art is useful\" - Dora Korpar,
 2015-10-19\n », 59);
 retour (1);
 }
