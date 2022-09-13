f3b5a14b262995e6607ad4fe309bb9632a229184
adamsbola <adamadu52@gmail.com>
2022-09-12 17:35:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <unistd.h>
+#inclure <stdio.h>
+#inclure <stdlib.h>
+#inclure <unistd.h>
 /**
- * main - Prints a text
- * Return: Always 1 (Success)
+ *principal - Point d’entrée
+ *Retour: Toujours 1 (Succès)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar,
-2015-10-19\n", 59);
-return (1);
-}
+write(STDOUT_FILENO, « et cette œuvre d’art est utile\ » - Dora Korpar,
+2015-10-19\n », 59);
+retour (1);
+
