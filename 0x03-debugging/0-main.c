7b883d7a62a4f43abb81c898ffea9b26b0f26419
adamsbola <adamadu52@gmail.com>
2022-09-14 09:42:25 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,13 @@
  * main - prints positive or negative integer
  * Return: 0
  */
-int main(void)																	
-{
+
+int main(void)																{
 	int i;
 
 	i = 0;
 	
 	positive_or_negative(i);
 
-	return (0);															}
+return (0);
+}
