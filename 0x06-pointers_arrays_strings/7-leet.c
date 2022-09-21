4dedebef95bc7b635e0e707151774bb4d10eaf01
Adamsbola <adamadu52@gmail.com>
2022-09-21 19:24:06 +0000

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,9 +13,9 @@ char *leet(char *s)
 
 	int i, j;
 
-	pour (j = 0; s[j] != '\0'; j++)
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		pour (i = 0; a[i] != '\0'; i++)
+		for (i = 0; a[i] != '\0'; i++)
 		{
 			si (s[j] == a[i])
 				s[j] = b[i];
