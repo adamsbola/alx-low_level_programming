4eac05fc8dbb76598743a475bfc34a38ee475011
adamsbola <adamadu52@gmail.com>
2023-01-05 19:53:34 +0100

    ok

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,17 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Get the index at which a key/value pair should
- *             be stored in array of a hash table.
- * @key: The key to get the index of.
- * @size: The size of the array of the hash table.
- *
- * Return: The index of the key.
- *
- * Description: Uses the djb2 algorithm.
+ * key_index - function that get the index where key/value is stored
+ * @key: type pointer char of key index
+ * @size: type pointer char the size of the array
+ * Return: Index of the key
  */
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
